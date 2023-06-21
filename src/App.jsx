7fedfbe033dda7ac85d0e1cc1ead430e08192ba9
Federico Marquez <fmarquez@aimonkey.io>
2023-06-21 18:50:58 -0300

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Counter from './components/Counter'
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
     <Header/>
     <Routes>
-      <Route path="/" element={<Counter/>} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/ejemplo" element={<Counter/>} />
     </Routes>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="h-16 bg-slate-500">Header</div>
+    <div className="h-16 sm:h-24 bg-slate-400">Header</div>
   )
 }
 
