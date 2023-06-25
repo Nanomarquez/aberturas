4e6905ded3cbc4f0974495f7f7a253a3d78cbb7a
Federico Marquez <fmarquez@beproactive.com.ar>
2023-06-25 15:59:34 -0300

    home 3 pantallas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
 import "./Button.css";
 import { Parallax } from "react-parallax";
-import bg from '../../public/images/zane-lee-ECsnJcc0Dhs-unsplash-scaled.jpg'
+import bg from "../../public/images/tecnoaberturasdc.jpg";
+import bgMobile from "../../public/images/r-architecture-updbdgKxp5k-unsplash-scaled.jpg";
+import bg2 from "../../public/images/339101016_5799821736793996_4235762734985220392_n (1).jpg";
+import bgMobile2 from "../../public/images/barandas-edificios.jpg";
+import { isMobile } from 'react-device-detect';
 
 function Home({ clicked }) {
   return (
@@ -8,7 +12,7 @@ function Home({ clicked }) {
       <section className="slider bg-[url('/images/zane-lee-ECsnJcc0Dhs-unsplash-scaled.jpg')] h-screen bg-cover bg-center">
         <div className="content">
           {!clicked && (
-            <div className=" flex flex-col items-center pt-60">
+            <div className="flex flex-col items-center justify-center h-screen">
               <h4
                 data-aos-duration="1000"
                 data-aos="zoom-in"
@@ -17,6 +21,7 @@ function Home({ clicked }) {
                 DISEÑO PERSONALIZADO
               </h4>
               <button
+              data-aos-delay="500"
                 data-aos-duration="1000"
                 data-aos="fade-up"
                 className="btn btn-1 hover-filled-slide-right"
@@ -27,9 +32,43 @@ function Home({ clicked }) {
           )}
         </div>
       </section>
-			<Parallax strength={-200} bgImage={bg} blur={{min:10,max:-10}} className="h-screen">
-
-			</Parallax>
+      <Parallax strength={isMobile ? 0 : -200} bgImage={isMobile ? bgMobile: bg} blur={{ min: 10, max: -10 }}>
+        <div className={`h-screen flex flex-col justify-center ${isMobile ? "text-black" : "text-white"} text-center sm:text-left sm:px-24 gap-12`}>
+          <p className="tracking-widest font-normal" data-aos-duration="1000"
+                data-aos="fade-right">
+            FÁBRICA DE ABERTURAS A MEDIDA EN PILAR
+          </p>
+          <h1 data-aos-delay="200" data-aos-duration="1000"
+                data-aos="fade-right" className="text-6xl font-semibold drop-shadow-md">
+            Tecno Aberturas DC
+          </h1>
+          <small data-aos-delay="400" data-aos-duration="1000"
+                data-aos="fade-right" className="text-lg">
+            Fabricantes de aberturas de alta prestación, modelos estándar y a
+            medida. Venta y colocación.{" "}
+          </small>
+          <button data-aos-delay="600" data-aos-duration="1000"
+                data-aos="fade-right" className="self-center btn btn-1 hover-filled-slide-right m-0 sm:self-start"> <span>Ver productos</span></button>
+        </div>
+      </Parallax>
+      <Parallax strength={isMobile ? 0 : -200} bgImage={isMobile ? bgMobile2: bg2} blur={{ min: 10, max: -10 }}>
+        <div className={`h-screen flex flex-col justify-center ${isMobile ? "text-black" : "text-white"} text-center sm:text-left sm:px-24 gap-12`}>
+          <p data-aos-duration="1000"
+                data-aos="fade-right" className="tracking-widest font-normal">
+          PRESUPUESTOS SIN CARGO
+          </p>
+          <h1 data-aos-delay="200" data-aos-duration="1000"
+                data-aos="fade-right" className="text-6xl font-semibold drop-shadow-md">
+          Realizamos todo proyecto
+          </h1>
+          <small data-aos-delay="400" data-aos-duration="1000"
+                data-aos="fade-right" className="text-lg">
+          Solicitanos el envío y la instalación completa de tus aberturas.{" "}
+          </small>
+          <button data-aos-delay="600" data-aos-duration="1000"
+                data-aos="fade-right" className="self-center btn btn-1 hover-filled-slide-right m-0 sm:self-start"> <span>Contacto</span></button>
+        </div>
+      </Parallax>
     </>
   );
 }
