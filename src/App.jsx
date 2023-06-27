c7ba21f39474c6e18c31dd1afd35cda1a99886db
Federico Marquez <fmarquez@beproactive.com.ar>
2023-06-27 00:45:48 -0300

    obras

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,40 @@
-import React, { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/NavBar';
-import Home from './components/Home';
-import Obras from './components/Obras';
-import Productos from './components/Productos';
-import Nosotros from './components/Nosotros';
-import Contacto from './components/Contacto';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/NavBar";
+import Home from "./components/Home";
+import Obras from "./components/Obras";
+import Productos from "./components/Productos";
+import Nosotros from "./components/Nosotros";
+import Contacto from "./components/Contacto";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
-	
-	const [clicked, setClicked] = React.useState(false);
-	const handleClick = () => {
-		//cuando esta true lo pasa a false y vice versa
-		setClicked(!clicked);
-	};
+  const [clicked, setClicked] = React.useState(false);
+  const handleClick = () => {
+    //cuando esta true lo pasa a false y vice versa
+    setClicked(!clicked);
+  };
 
-	useEffect(() => {
-		AOS.init();
-	}, [])
+  useEffect(() => {
+    AOS.init({
+      mirror: true,
+    });
+  }, []);
 
-	return (
-		<>
-			<Header
-				clicked={clicked}
-				handleClick={handleClick}
-			/>
-			<Routes>
-				<Route
-					path="/"
-					element={<Home clicked={clicked} />}
-				/>
-				<Route
-					path="/productos"
-					element={<Productos />}
-				/>
-				<Route
-					path="/nosotros"
-					element={<Nosotros />}
-				/>
-				<Route
-					path="/obras"
-					element={<Obras />}
-				/>
-				<Route
-					path="/contacto"
-					element={<Contacto />}
-				/>
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Header clicked={clicked} handleClick={handleClick} />
+      <Routes>
+        <Route path="/" element={<Home clicked={clicked} />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/obras" element={<Obras />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/obras" element={<Obras />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
