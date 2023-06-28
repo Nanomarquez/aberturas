5b0480d30a869e4050ae636e4d07e2aa29516cb5
Federico Marquez <fmarquez@beproactive.com.ar>
2023-06-28 17:00:38 -0300

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,7 @@ import Obras from './components/Obras';
 import Productos from './components/Productos';
 import Nosotros from './components/Nosotros';
 import Contacto from './components/Contacto';
-<<<<<<< HEAD
 import Footer from './components/Footer';
-=======
->>>>>>> d296279118dd4ce0e9f56411afc37bb3796d63ae
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -24,11 +21,7 @@ function App() {
 
 	useEffect(() => {
 		AOS.init();
-<<<<<<< HEAD
 	}, []);
-=======
-	}, [])
->>>>>>> d296279118dd4ce0e9f56411afc37bb3796d63ae
 
 	return (
 		<>
