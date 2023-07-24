f1359c779e9b03875448eb796a62c3768c4503e5
fabiancord <fabianarielcordobes@gmail.com>
2023-07-24 16:33:17 -0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-import Header from './components/NavBar';
-import Home from './components/Home';
-import Obras from './components/Obras';
-import Productos from './components/Productos';
-import Nosotros from './components/Nosotros';
-import Contacto from './components/Contacto';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-=======
 import Header from './components/Navbar/NavBar';
 import Home from './pages/Home/Home';
 import Obras from './pages/Obras/Obras';
@@ -24,7 +14,6 @@ import Cortinas from './pages/Cortinas/Cortinas';
 import Cerramientos from './pages/Cerramientos/Cerramientos';
 import Pergolas from './pages/Pergolas/Pergolas';
 import Mamparas from './pages/Mamparas/Mamparas';
->>>>>>> 59a320b401c426612c6a95719cab790f57d60bbe
 
 function App() {
 	const [clicked, setClicked] = React.useState(false);
